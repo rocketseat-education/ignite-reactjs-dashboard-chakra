b3b2f093d1084b7c11d5df9b89ad8e8b41699741
Diego Fernandes <diego.schell.f@gmail.com>
2021-03-29 14:21:22 -0300

    Configurando fonte

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
