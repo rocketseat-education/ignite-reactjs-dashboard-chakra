1e3d70740124b9f3723a8fd2c1b6bbf25db07bdd
Diego Fernandes <diego.schell.f@gmail.com>
2021-03-29 15:21:13 -0300

    Finalizando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -13,12 +13,7 @@ export function Header() {
       px="6"
       align="center"
     >
-      <Text
-        fontSize="3xl"
-        fontWeight="bold"
-        letterSpacing="tight"
-        w="64"
-      >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
         dashgo
         <Text as="span" ml="1" color="pink.500">.</Text>
       </Text>
@@ -46,6 +41,32 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+    
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+      
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Diego Fernandes</Text>
+            <Text color="gray.300" fontSize="small">
+              diego.schell.f@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Diego Fernandes" src="https://github.com/diego3g.png" />
+        </Flex>
       </Flex>
     </Flex>
   );
