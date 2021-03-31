0ef44e6da118b8780ad79041a930ec5f2b3117c6
Diego Fernandes <diego.schell.f@gmail.com>
2021-03-31 10:24:01 -0300

    Formularios no React

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,7 +1,19 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
+// Controlled components
+// Uncontrolled components
+
+// Imperativa vs Declarativa
+
 export function SearchBox() {
+  // const [search, setSearch] = useState('')
+
+  const searchInputRef = useRef<HTMLInputElement>(null)
+
+  // searchInputRef.current.focus() (Imperativo)
+
   return (
     <Flex
       as="label"
@@ -23,6 +35,7 @@ export function SearchBox() {
         mr="4"
         placeholder="Buscar na plataforma"
         _placeholder={{ color: 'gray.400' }}
+        ref={searchInputRef}
       />
 
       <Icon as={RiSearchLine} fontSize="20" />
