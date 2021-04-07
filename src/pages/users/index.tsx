85785973a6f79398b28a7940bb8630cabde98a8b
Diego Fernandes <diego.schell.f@gmail.com>
2021-04-07 12:17:21 -0300

    Configurando Axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,11 +6,11 @@ import { useQuery } from 'react-query'
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+    const { data } = await api.get('users')
   
     const users = data.users.map(user => {
       return {
