7d567601757a991fef3dd5de3b0a51a76da2ebf8
Diego Fernandes <diego.schell.f@gmail.com>
2021-04-07 11:45:57 -0300

    Listagem de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,7 +13,20 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
   
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      };
+    });
+
+    return users;
   })
 
   const isWideVersion = useBreakpointValue({
@@ -66,42 +79,22 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Diego Fernandes</Text>
-                        <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>04 de Abril, 2021</Td> }
-                  </Tr>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Diego Fernandes</Text>
-                        <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>04 de Abril, 2021</Td> }
-                  </Tr>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Diego Fernandes</Text>
-                        <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>04 de Abril, 2021</Td> }
-                  </Tr>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        { isWideVersion && <Td>{user.createdAt}</Td> }
+                      </Tr>
+                    )
+                  })}
                 </Tbody>
               </Table>
 
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -28,7 +28,7 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList('user', 200)
+      server.createList('user', 10)
     },
 
     routes() {
